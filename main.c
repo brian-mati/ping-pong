72178570781e5228c0ac2999e8ee2813d9553d53
brian-mati <brianmuthomimati@gmail.com>
2025-07-22 15:33:53 +0300

    feat:player movement logic added

--- main.c
+++ main.c
@@ -3,23 +3,48 @@
 #include <stdio.h>
 #include <raylib.h>
 
+  /*
+   * Todos 
+   * 1. Define player instances (2 players)
+   * 2. Define ball instances
+   * 3. Define player and ball movement 
+   * 4. Define collisions
+   */
+
+ struct PlayerInstance {
+  float positionX;
+  float positionY;
+  int   score ; 
+};
+
+typedef struct PlayerInstance Player;
+
+
 int main(){
+  Player firstPlayer;
 	const int screenWidth = 800;
 	const int screenHeight = 450;
 	const int runningFps = GetFPS();
-
+  
+  firstPlayer.positionX;
+  firstPlayer.positionY;
+  
+  Vector2 firstPlayerPosition = { (float)screenWidth/2, (float)screenHeight/2 }; 
+  Vector2 playerSize = { 20,100 };
 	InitWindow(screenWidth,screenHeight,"ping pong game");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
 	{
-		int gameFps = 0;
-		gameFps = gameFps + runningFps;
-		// printf("current-frames:%d\n",gameFps); #todo - fix fps counter in stdout
+    if (IsKeyDown(KEY_RIGHT)) firstPlayerPosition.x += 2.0f; 
+    if (IsKeyDown(KEY_LEFT)) firstPlayerPosition.x -= 2.0f;
+
+    if (IsKeyDown(KEY_UP)) firstPlayerPosition.y -= 2.0f;
+    if (IsKeyDown(KEY_DOWN)) firstPlayerPosition.y += 2.0f;
+    
 		BeginDrawing();
 			ClearBackground(BLACK);
-			DrawRectangle(40, 40 ,  100 , 200 , WHITE);
-			DrawCircle(400,100,40,DARKBLUE);
+			DrawRectangleV(firstPlayerPosition, playerSize , WHITE);
 		EndDrawing();
 	}
     return 0;
